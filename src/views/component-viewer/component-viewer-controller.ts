a1cfda2229693389e895e43fc4540baf7c0d3315
Omar Elkhouly <omar.elkhouly@arm.com>
2026-01-13 17:08:19 +0100

    maintaining state of removing mocks from controller, will remove mocks from other files in a separate commit

--- src/views/component-viewer/component-viewer-controller.ts
+++ src/views/component-viewer/component-viewer-controller.ts
@@ -18,52 +18,13 @@ import * as vscode from 'vscode';
 import { GDBTargetDebugTracker, GDBTargetDebugSession, SessionStackItem } from '../../debug-session';
 import { ComponentViewerInstance } from './component-viewer-instance';
 import { URI } from 'vscode-uri';
-import path from 'path';
 import { ComponentViewerTreeDataProvider } from './component-viewer-tree-view';
-
-const scvdMockTestFiles: Map<string, boolean> = new Map<string, boolean>([
-    ['src/component-viewer/test/test-files/scvd/MinimalTest.scvd',      true],
-    ['src/component-viewer/test/test-files/scvd/MyTest.scvd',           false],
-    ['src/component-viewer/test/test-files/scvd/RTX5.scvd',             false],
-    ['src/component-viewer/test/test-files/scvd/BaseExample.scvd',      false],
-    ['src/component-viewer/test/test-files/scvd/Network.scvd',          false],
-    ['src/component-viewer/test/test-files/scvd/USB.scvd',              false],
-    ['src/component-viewer/test/test-files/scvd/FileSystem.scvd',       false],
-    ['src/component-viewer/test/test-files/scvd/EventRecorder.scvd',    false],
-    ['src/component-viewer/test/test-files/scvd/GetRegVal_Test.scvd',   false],
-]);
-
-const scvdMockFiles: string[] = Array.from(scvdMockTestFiles.entries())
-    .filter(([_, include]) => include)
-    .map(([filePath]) => filePath);
-
-// Helper function to create a mock GDBTargetDebugSession for testing
-export function createMockDebugSession(): GDBTargetDebugSession {
-    const mockVSCodeSession: vscode.DebugSession = {
-        id: 'mock-session-id',
-        name: 'Mock Debug Session',
-        type: 'gdbtarget',
-        workspaceFolder: undefined,
-        configuration: {
-            type: 'gdbtarget',
-            name: 'Mock Debug Session',
-            request: 'launch'
-        },
-        customRequest: async () => ({}),
-        getDebugProtocolBreakpoint: async () => undefined
-    };
-    return new GDBTargetDebugSession(mockVSCodeSession);
-}
-
 
 export class ComponentViewerController {
     private activeSession: GDBTargetDebugSession | undefined;
     private instances: ComponentViewerInstance[] = [];
     private componentViewerTreeDataProvider: ComponentViewerTreeDataProvider | undefined;
     private _context: vscode.ExtensionContext;
-    private mockFlag: boolean = true;
-    // Check if mocks shall be used from configuration
-    private useMocks = vscode.workspace.getConfiguration('vscode-cmsis-debugger').get('useMocks');
 
     public constructor(context: vscode.ExtensionContext) {
         this._context = context;
@@ -75,45 +36,8 @@ export class ComponentViewerController {
         const treeProviderDisposable = vscode.window.registerTreeDataProvider('cmsis-debugger.componentViewer', this.componentViewerTreeDataProvider);
         this._context.subscriptions.push(
             treeProviderDisposable);
-        // Subscribe to vscode event for changes in mocks configuration in the settings.json
-        vscode.workspace.onDidChangeConfiguration(async (event) => {
-            // Do an initial setup of mocks if useMocks is true and no instances are loaded yet
-            this.useMocks = vscode.workspace.getConfiguration('vscode-cmsis-debugger').get('useMocks');
-            if (this.instances.length === 0 && this.useMocks && this.mockFlag) {
-                this.mockFlag = false; // to avoid multiple initializations
-                await this.useMocksInstances(this._context);
-                await this.componentViewerTreeDataProvider?.activate();
-                return;
-            }
-            // if there are instances already loaded, check if the configuration changed
-            if (event.affectsConfiguration('vscode-cmsis-debugger.useMocks')) {
-                if (this.useMocks) {
-                    await this.useMocksInstances(this._context);
-                    await this.componentViewerTreeDataProvider?.activate();
-                } else {
-                    await this.componentViewerTreeDataProvider?.deleteModels();
-                }
-            }
-        });
         // Subscribe to debug tracker events to update active session
         this.subscribetoDebugTrackerEvents(this._context, tracker);
-    }
-
-    protected async buildMockInstancesArray(context: vscode.ExtensionContext): Promise<void> {
-        const mockedInstances: ComponentViewerInstance[] = [];
-        //const mockSession = createMockDebugSession();
-        for (const scvdFile of scvdMockFiles) {
-            const instance = new ComponentViewerInstance();
-            try {
-                // use a mocked GDBTargetDebugSession
-                await instance.readModel(URI.file(path.join(context.extensionPath, scvdFile)), createMockDebugSession(), new GDBTargetDebugTracker());
-            } catch (error) {
-                console.error('Error reading mock SCVD file:', scvdFile, error);
-                continue;
-            }
-            mockedInstances.push(instance);
-        }
-        this.instances = mockedInstances;
     }
 
     protected async readScvdFiles(tracker: GDBTargetDebugTracker,session?: GDBTargetDebugSession): Promise<void> {
@@ -140,14 +64,6 @@ export class ComponentViewerController {
         this.instances = cbuildRunInstances;
     }
 
-    private async useMocksInstances(context: vscode.ExtensionContext) : Promise<void> {
-        await this.buildMockInstancesArray(context);
-        // Add all mock models to the tree view
-        for (const instance of this.instances) {
-            await instance.update();
-            this.componentViewerTreeDataProvider?.addGuiOut(instance.getGuiTree());
-        }
-    }
 
     private loadingCounter: number = 0;
     private async loadCbuildRunInstances(session: GDBTargetDebugSession, tracker: GDBTargetDebugTracker) : Promise<void> {
@@ -216,11 +132,6 @@ export class ComponentViewerController {
     }
 
     private async handleOnConnected(session: GDBTargetDebugSession, tracker: GDBTargetDebugTracker): Promise<void> {
-        // If mocks are being used, erase them and start reading SCVD files from cbuild-run
-        if ( this.instances.length > 0 && this.useMocks ) {
-            this.instances = [];
-            await this.componentViewerTreeDataProvider?.deleteModels();
-        }
         // if new session is not the current active session, erase old instances and read the new ones
         if (this.activeSession?.session.id !== session.session.id) {
             this.instances = [];
