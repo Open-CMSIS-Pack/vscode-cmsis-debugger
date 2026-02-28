869cf12c6254ea4e626d9f61dc0bd689a9c36a44
Jens Reinecke <jens.reinecke@arm.com>
2026-02-28 10:56:00 +0100

    ComponentViewerScvdCollector tests
    Signed-off-by: Jens Reinecke <jens.reinecke@arm.com>

--- src/views/component-viewer/test/unit/component-viewer-scvd-collector.test.ts
+++ src/views/component-viewer/test/unit/component-viewer-scvd-collector.test.ts
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+import { gdbTargetDebugSessionFactory } from '../../../../debug-session/__test__/debug-session.factory';
 import { ComponentViewerScvdCollector } from '../../component-viewer-scvd-collector';
 
 describe('ComponentViewerScvdCollector', () => {
@@ -23,8 +24,30 @@ describe('ComponentViewerScvdCollector', () => {
         componentViewerScvdCollector = new ComponentViewerScvdCollector();
     });
 
-    it('exists', () => {
-        expect(componentViewerScvdCollector).toBeDefined();
+    it.each([
+        {
+            inputFilePaths: ['path/to/scvd1.scvd', 'path/to/scvd2.scvd'],
+            pname: undefined,
+        },
+        {
+            inputFilePaths: [],
+            pname: 'test-pname',
+        }
+    ])('returns scvd files as registered in the session', async ({ inputFilePaths, pname }) => {
+        const session = gdbTargetDebugSessionFactory('test-session-id', inputFilePaths, 'unknown', pname);
+        const cbuildRunReader = await session.getCbuildRun();
+        expect(cbuildRunReader).toBeDefined();
+        const cbuildRunGetScvdSpy = jest.spyOn(cbuildRunReader!, 'getScvdFilePaths');
+        const receivedPaths = await componentViewerScvdCollector.getScvdFilePaths(session);
+        expect(cbuildRunGetScvdSpy).toHaveBeenCalledWith(undefined, pname);
+        expect(receivedPaths).toEqual(expect.arrayContaining(inputFilePaths));
+    });
+
+    it('returns empty array if cbuildRun reader is undefined', async () => {
+        const inputFilePaths = ['path/to/scvd1.scvd', 'path/to/scvd2.scvd'];
+        const session = gdbTargetDebugSessionFactory('test-session-id', inputFilePaths, 'unknown', undefined, false);
+        const receivedPaths = await componentViewerScvdCollector.getScvdFilePaths(session);
+        expect(receivedPaths).toEqual([]);
     });
 
 });
