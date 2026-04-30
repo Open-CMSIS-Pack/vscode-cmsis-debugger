e9f272007398677589c81fa493d4d1d4df61402a
Omar Elkhouly <omar.elkhouly@arm.com>
2026-04-30 13:51:49 +0200

    supporting setting values in live watch

--- src/views/live-watch/live-watch.ts
+++ src/views/live-watch/live-watch.ts
@@ -152,6 +152,9 @@ export class LiveWatchTreeDataProvider implements vscode.TreeDataProvider<LiveWa
         const refreshCommand = vscode.commands.registerCommand('vscode-cmsis-debugger.liveWatch.refresh', async () => await this.refresh());
         const modifyCommand = vscode.commands.registerCommand('vscode-cmsis-debugger.liveWatch.modify', async (node) => await this.handleRenameCommand(node));
         const copyCommand = vscode.commands.registerCommand('vscode-cmsis-debugger.liveWatch.copy', async (node) => await this.handleCopyCommand(node));
+        const setValueCommand = vscode.commands.registerCommand('vscode-cmsis-debugger.liveWatch.setValue', async (node) => {
+            await this.handleSetValueCommand(node);
+        });
         const addToLiveWatchCommand = vscode.commands.registerCommand('vscode-cmsis-debugger.liveWatch.addToLiveWatchFromTextEditor',
             async () => await this.handleAddFromSelectionCommand());
         /* omarArm: I am using the same callback function for both watch window and variables view, as they have the same payload structure for now.
@@ -174,6 +177,7 @@ export class LiveWatchTreeDataProvider implements vscode.TreeDataProvider<LiveWa
             refreshCommand,
             modifyCommand,
             copyCommand,
+            setValueCommand,
             addToLiveWatchCommand,
             addToLiveWatchFromWatchWindowCommand,
             addToLiveWatchFromVariablesViewCommand,
@@ -282,6 +286,32 @@ export class LiveWatchTreeDataProvider implements vscode.TreeDataProvider<LiveWa
         response.variablesReference = result.variablesReference;
         response.type = result.type ?? '';
         return response;
+    }
+
+    private async handleSetValueCommand(node: LiveWatchNode) {
+        if (!node) {
+            return;
+        }
+        const newValue = await vscode.window.showInputBox({ prompt: 'New Value', value: node.value.result });
+        if (newValue === undefined) {
+            return;
+        }
+        // VSCode sends setExpression requests for parent nodes, and setVariable requests for child nodes. We can use the presence of parent to determine which request to send.
+        if (node.parent) {
+            await this._activeSession?.session.customRequest('setVariable', {
+                name: node.expression,
+                value: newValue,
+                variablesReference: node.parent.value.variablesReference
+            });
+        } else {
+            const frameId = (vscode.debug.activeStackItem as vscode.DebugStackFrame)?.frameId ?? 0;
+            await this._activeSession?.session.customRequest('setExpression', {
+                expression: node.expression,
+                value: newValue,
+                frameId: frameId
+            });
+        }
+        await this.refresh(node);
     }
 
     private async addToRoots(expression: string, parent?: LiveWatchNode) {
