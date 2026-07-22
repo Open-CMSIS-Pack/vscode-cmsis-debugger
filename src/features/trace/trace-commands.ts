fa7d37efb6e858370cf30c52c12c4b048de58d81
Jens Reinecke <jens.reinecke@arm.com>
2026-07-22 08:58:33 +0200

    Make trace commands use expected files (to be refactored)
    Signed-off-by: Jens Reinecke <jens.reinecke@arm.com>

--- src/features/trace/trace-commands.ts
+++ src/features/trace/trace-commands.ts
@@ -18,12 +18,17 @@ import * as vscode from 'vscode';
 import { EXTENSION_NAME } from '../../manifest';
 import { PyTsProcessManager } from '../../desktop/process/pyts-process-manager';
 import { CTraceProcessManager } from '../../desktop/process/ctrace-process-manager';
+import { BuiltinToolPath } from '../../desktop/builtin-tool-path';
+
+const PYTS_BUILTIN_PATH = 'tools/pyts/pyts';
+const CTRACE_BUILTIN_PATH = 'tools/ctrace/ctrace';
 
 /**
  * Registers commands related to trace functionality.
  */
 export class TraceCommands {
     public static readonly CSOLUTION_GET_CBUILD_RUN_FILE_COMMAND = 'cmsis-csolution.getCbuildRunFile';
+    public static readonly CSOLUTION_GET_ACTIVE_TARGET_SET_COMMAND = 'cmsis-csolution.getActiveTargetSet';
 
     public static readonly launchPyTsID = `${EXTENSION_NAME}.launchPyTs`;
     public static readonly launchCTraceID = `${EXTENSION_NAME}.launchCTrace`;
@@ -43,7 +48,15 @@ export class TraceCommands {
             if (!trimmedPath) {
                 throw new Error('No cbuild run file path provided.');
             }
-            const pytsProcessManager = new PyTsProcessManager({ cbuildRunFilePath: trimmedPath });
+            const builtInPyTs = new BuiltinToolPath(PYTS_BUILTIN_PATH);
+            const pyTsAbsolutePath = builtInPyTs.getAbsolutePath();
+            if (!pyTsAbsolutePath) {
+                throw new Error('Failed to resolve the absolute path for pyTS.');
+            }
+            const pytsProcessManager = new PyTsProcessManager({
+                cbuildRunFilePath: trimmedPath,
+                pyTsPath: pyTsAbsolutePath.fsPath
+            });
             pytsProcessManager.launch();
             await pytsProcessManager.waitForExit();
         } catch (error) {
@@ -52,8 +65,31 @@ export class TraceCommands {
     }
 
     protected async handleLaunchCTrace(): Promise<void> {
-        const ctraceProcessManager = new CTraceProcessManager({});
         try {
+            const path = await vscode.commands.executeCommand<string | undefined>(TraceCommands.CSOLUTION_GET_CBUILD_RUN_FILE_COMMAND);
+            const trimmedPath = path?.trim();
+            if (!trimmedPath) {
+                throw new Error('No cbuild run file path provided.');
+            }
+            const solutionMatches = trimmedPath.match(/.*[\\/](.*)\+.*\.cbuild-run\.yml$/);
+            const solutionName = solutionMatches?.[1];
+            if (!solutionName) {
+                throw new Error('Failed to extract solution name from cbuild run file path.');
+            }
+            const activeSet = await vscode.commands.executeCommand<string | undefined>(TraceCommands.CSOLUTION_GET_ACTIVE_TARGET_SET_COMMAND);
+            const trimmedActiveSet = activeSet?.trim();
+            const targetSet = trimmedActiveSet ? `+${trimmedActiveSet}` : '';
+            const workspacePath = vscode.workspace.workspaceFolders?.at(0)?.uri.fsPath;
+            const rawFilePath = `${workspacePath}/.trace/${solutionName}${targetSet}.SWO.raw`;
+            const builtInCTrace = new BuiltinToolPath(CTRACE_BUILTIN_PATH);
+            const cTraceAbsolutePath = builtInCTrace.getAbsolutePath();
+            if (!cTraceAbsolutePath) {
+                throw new Error('Failed to resolve the absolute path for ctrace.');
+            }
+            const ctraceProcessManager = new CTraceProcessManager({
+                rawFilePath,
+                cTracePath: cTraceAbsolutePath.fsPath
+            });
             ctraceProcessManager.launch();
             await ctraceProcessManager.waitForExit();
         } catch (error) {
